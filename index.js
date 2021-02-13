5095ab2fde46ac2a518668f47a164e7db97d6564
Adam Argyle <atom@argyleink.com>
2021-02-12 22:34:45 -0800

    migration done

--- index.js
+++
@@ -1,9 +0,0 @@
-import confetti from 'canvas-confetti'
-import $ from 'blingblingjs'
-
-confetti.create(null, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 })
-
-console?.log('ready.')
